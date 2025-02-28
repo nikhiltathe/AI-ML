1ee0cade0d719fcc2b3dbbaa4701dd0e42d37dd0
nikhil5987 <nikhil.tathe@gmail.com>
2025-02-28 20:17:02 +0530

    Move to Folder 1

---
+++ 1/Housing_Prices_Competition_for_Kaggle_Learn_Users/solution.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+file_path = 'C:/Users/tathen/PycharmProjects/ML/AI-ML/Housing_Prices_Competition_for_Kaggle_Learn_Users/home-data-for-ml-course/train.csv'
+data = pd.read_csv(file_path)
+print(data.columns)
+
